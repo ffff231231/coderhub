5e368df5b150620615004d404930e7ad8909cd2f
FFFF231 <jiadenghui231@gmail.com>
2023-04-23 17:27:27 +0800

    编写.env文件，并通过dotenv加载配置的环境变量

--- src/main.js
+++ src/main.js
@@ -1,1 +1,16 @@
-console.log("项目执行代码~")
+const Koa = require('koa')
+const KoaRouter = require('@koa/router')
+const { SERVER_RORT } = require('./config/server')
+
+const app = new Koa()
+
+const userRouter = new KoaRouter({ prefix: '/users' })
+userRouter.get('/list', (ctx, next) => {
+  ctx.body = 'users list'
+})
+app.use(userRouter.routes())
+app.use(userRouter.allowedMethods())
+
+app.listen(SERVER_RORT, () => {
+  console.log('coderhub的服务器启动成功~')
+})
