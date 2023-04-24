1cbb59dfaa17e8b2a56ec1c87b5906ef4ab45151
FFFF231 <jiadenghui231@gmail.com>
2023-04-24 23:16:04 +0800

    动态注册所有的路由对象

---
+++ src/router/index.js
@@ -0,0 +1,17 @@
+const fs = require('fs')
+
+function registerRouters(app) {
+  // 1.读取当前文件夹下的所有文件
+  const files = fs.readdirSync(__dirname)
+  console.log(files)
+
+  // 2.遍历所有的文件
+  for (const file of files) {
+    if (!file.endsWith('.router.js')) continue
+    const router = require(`./${file}`)
+    app.use(router.routes())
+    app.use(router.allowedMethods())
+  }
+}
+
+module.exports = registerRouters
